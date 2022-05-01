1bf670b54219ea22bf5665e03117ad0ec70b65a9
Pamesch21 <93684844+Pamesch21@users.noreply.github.com>
2022-05-01 14:24:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+;
+
+for (i = 0; i <= dias.length - 1; i++ ){
+    document.write(dias [i]+ "<br>");
+   }
+
+   if ( dias == "lunes","martes","miercoles","jueves","viernes","sabado","domingo") 
+    
+{
+    alert("DIAS DE LA SEMANA")
+}
